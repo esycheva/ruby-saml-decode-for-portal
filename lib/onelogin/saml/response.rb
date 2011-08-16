f87c0e59139997d2a21f9ade06c43f4b6bdbb235
Елена Сычёва <esycheva@at-consulting.ru>
2011-08-16 11:43:54 +0400

    Изменен метод attributes, изменен метод name_id, удалены не используемые методы

--- lib/onelogin/saml/response.rb
+++ lib/onelogin/saml/response.rb
@@ -25,36 +25,16 @@ module Onelogin::Saml
 
     # The value of the user identifier as designated by the initialization request response
     def name_id
-      @name_id ||= document.elements["/samlp:Response/saml:Assertion/saml:Subject/saml:NameID"].text
+      @name_id ||= document.elements["saml2:Assertion/saml2:Subject/saml2:NameID"].text
     end
 
-    # A hash of alle the attributes with the response. Assuming there is onlye one value for each key
+    # A hash of attributes and values
     def attributes
-      saml_attribute_statements = document.elements["/samlp:Response/saml:Assertion/saml:AttributeStatement"].elements
-      statements = saml_attribute_statements.map do |child|
-        child.attributes.map do |key, attribute|
-          [attribute, child.elements.first.text]
-        end
+      result = {}
+      document.elements.each("saml2:Assertion/saml2:AttributeStatement/saml2:Attribute") do |element|
+        result.merge!(element.elements.first.text => element.attributes["FriendlyName"])
       end
-
-      hash = Hash[statements.flatten(1)]
-      @attributes ||= make_hash_access_indiferent(hash)
-    end
-
-    # When this user session should expire at latest
-    def session_expires_at
-      @expires_at ||= Time.parse(document.elements["/samlp:Response/saml:Assertion/saml:AuthnStatement"].attributes["SessionNotOnOrAfter"])
-    end
-
-  private
-
-    def make_hash_access_indiferent(hash)
-      sym_hash = {}
-      hash.each  do |key, value|
-        sym_hash[key.intern] = value
-      end
-
-      sym_hash.merge(hash)
+      result
     end
   end
 end
